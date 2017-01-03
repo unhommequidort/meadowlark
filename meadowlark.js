4ab3ae5186cf90f2f95128ad13d6ef3022206f5a
Matthew Lyons <matthewjosephlyons@gmail.com>
2017-01-02 19:29:09 -0600

    Moved 'fortune cookie' functionality into module.

--- meadowlark.js
+++ meadowlark.js
@@ -1,5 +1,5 @@
 var express = require('express');
-
+var fortune = require('./lib/fortune.js');
 var app = express();
 
 // set up handlebars view engine
@@ -16,16 +16,8 @@ app.get('/', function(req, res) {
     res.render('home');
 });
 
-var fortunes = [
-"Conquer your fears or they will conquer you.", "Rivers need springs.",
-"Do not fear what you don't know.",
-"You will have a pleasant surprise.", "Whenever possible, keep it simple.",
-];
-
 app.get('/about', function(req, res) {
-	var randomFortune =
-		fortunes[Math.floor(Math.random() * fortunes.length)];
-    res.render('about', { fortune: randomFortune });
+    res.render('about', { fortune: fortune.getFortune() });
 });
 
 // custom 404 page
